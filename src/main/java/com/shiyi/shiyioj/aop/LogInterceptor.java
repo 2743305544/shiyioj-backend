2f3291727fc5898f325e7bcb4ce65a3017a7e5be
SHIYI <3401187804@qq.com>
2024-12-27 21:59:28 +0800

    后端项目初始化

---
+++ src/main/java/com/shiyi/shiyioj/aop/LogInterceptor.java
@@ -0,0 +1,98 @@
+package com.shiyi.shiyioj.aop;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+
+import cn.hutool.json.JSONUtil;
+import com.shiyi.shiyioj.model.LogModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * 请求响应日志 AOP
+ *
+ * @author <a href="https://github.com/lishiyi">程序员鱼皮</a>
+ * @from <a href="https://shiyi.icu">编程导航知识星球</a>
+ **/
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LogInterceptor {
+
+    /**
+     * 执行拦截
+     */
+//    @Around("execution(* com.shiyi.shiyioj.controller.*.*(..))")
+//    public Object doInterceptor(ProceedingJoinPoint point) throws Throwable {
+//        // 计时
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        // 获取请求路径
+//        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+//        HttpServletRequest httpServletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+//        // 生成请求唯一 id
+//        String requestId = UUID.randomUUID().toString();
+//        String url = httpServletRequest.getRequestURI();
+//        // 获取请求参数
+//        Object[] args = point.getArgs();
+//        String reqParam = "[" + StringUtils.join(args, ", ") + "]";
+//        // 输出请求日志
+//        log.info("request start，id: {}, path: {}, ip: {}, params: {}", requestId, url,
+//                httpServletRequest.getRemoteHost(), reqParam);
+//        // 执行原方法
+//        Object result = point.proceed();
+//        // 输出响应日志
+//        stopWatch.stop();
+//        long totalTimeMillis = stopWatch.getTotalTimeMillis();
+//        log.info("request end, id: {}, cost: {}ms", requestId, totalTimeMillis);
+//        return result;
+//    }
+    private final HttpServletRequest request;
+
+    /**
+     * 监听使用了RestController注解的类
+     *
+     * @param point 切点
+     * @return 接口返回结果
+     */
+    @Around("@within(org.springframework.web.bind.annotation.RestController)")
+    public Object apiLog(ProceedingJoinPoint point) throws Throwable {
+        String requestUri = request.getRequestURI();
+        String requestIp = request.getRemoteAddr();
+        LocalDateTime requestTime = LocalDateTime.now();
+        Throwable exceptionInfo = null;
+        Object proceed = null;
+        try {
+            proceed = point.proceed();
+            return proceed;
+        } catch (Throwable e) {
+            exceptionInfo = e;
+            throw e;
+        } finally {
+            //打印日志
+            new LogModel(requestUri,
+                    point.getSignature().toString(),
+                    Arrays.toString(point.getArgs()),
+                    Objects.isNull(proceed) ? null : JSONUtil.toJsonStr(proceed),
+                    Objects.isNull(exceptionInfo) ? null : exceptionInfo.toString(),
+                    requestTime,
+                    LocalDateTime.now(),
+                    requestIp).
+                    log();
+        }
+    }
+}
+
---
+++ src/main/java/com/shiyi/shiyioj/wxmp/WxMpMsgRouter.java
@@ -0,0 +1,61 @@
+package com.shiyi.shiyioj.wxmp;
+
+import com.shiyi.shiyioj.wxmp.handler.EventHandler;
+import com.shiyi.shiyioj.wxmp.handler.MessageHandler;
+import com.shiyi.shiyioj.wxmp.handler.SubscribeHandler;
+import javax.annotation.Resource;
+import me.chanjar.weixin.common.api.WxConsts.EventType;
+import me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 微信公众号路由
+ *
+ * @author <a href="https://github.com/lishiyi">程序员鱼皮</a>
+ * @from <a href="https://shiyi.icu">编程导航知识星球</a>
+ */
+@Configuration
+public class WxMpMsgRouter {
+
+    @Resource
+    private WxMpService wxMpService;
+
+    @Resource
+    private EventHandler eventHandler;
+
+    @Resource
+    private MessageHandler messageHandler;
+
+    @Resource
+    private SubscribeHandler subscribeHandler;
+
+    @Bean
+    public WxMpMessageRouter getWxMsgRouter() {
+        WxMpMessageRouter router = new WxMpMessageRouter(wxMpService);
+        // 消息
+        router.rule()
+                .async(false)
+                .msgType(XmlMsgType.TEXT)
+                .handler(messageHandler)
+                .end();
+        // 关注
+        router.rule()
+                .async(false)
+                .msgType(XmlMsgType.EVENT)
+                .event(EventType.SUBSCRIBE)
+                .handler(subscribeHandler)
+                .end();
+        // 点击按钮
+        router.rule()
+                .async(false)
+                .msgType(XmlMsgType.EVENT)
+                .event(EventType.CLICK)
+                .eventKey(WxMpConstant.CLICK_MENU_KEY)
+                .handler(eventHandler)
+                .end();
+        return router;
+    }
+}
