b91fe460f94453754690067da545fcd32ff32d19
SHIYI <3401187804@qq.com>
2024-12-28 20:30:25 +0800

    修改数据库sql文件

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://shiyi.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists shiyioj;
 
 -- 切换库
-use my_db;
+use shiyioj;
 
 -- 用户表
 create table if not exists user
@@ -26,31 +26,41 @@ create table if not exists user
     index idx_unionId (unionId)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 帖子表
-create table if not exists post
+-- 题目表
+create table if not exists question
 (
-    id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
-    content    text                               null comment '内容',
-    tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    thumbNum   int      default 0                 not null comment '点赞数',
-    favourNum  int      default 0                 not null comment '收藏数',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(512)                       null comment '标题',
+    content     text                               null comment '内容',
+    tags        varchar(1024)                      null comment '标签列表（json 数组）',
+    answer      text                               null comment '答案',
+    submitNum   int      default 0                 not null null comment '题目提交数',
+    acceptNum   int      default 0                 not null null comment '题目通过数',
+    judgeCase   text                               null comment '测试用例(json 数组)',
+    judgeConfig text                               null comment '评测配置 (json 对象)',
+    thumbNum    int      default 0                 not null comment '点赞数',
+    favourNum   int      default 0                 not null comment '收藏数',
+    userId      bigint                             not null comment '创建用户 id',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
     index idx_userId (userId)
 ) comment '帖子' collate = utf8mb4_unicode_ci;
 
--- 帖子点赞表（硬删除）
-create table if not exists post_thumb
+-- 题目提交表
+create table if not exists question_submit
 (
     id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
+    language   varchar(128)                       not null comment '提交语言',
+    questionId int                                not null comment '题目 id',
+    code       text                               not null comment '代码',
+    judgeInfo  text                               null comment '判题信息(json 数组)',
+    status     int  default 0                     not null comment '状态：0-未判题，1-判题中 2-通过，3-未通过',
+    userId     bigint                             not null comment '用户 id',
     createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    index idx_postId (questionId),
     index idx_userId (userId)
 ) comment '帖子点赞';
 
